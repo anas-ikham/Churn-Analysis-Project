11a9b27c1f0fc7e6d5b41a8e0675b805c9a74ad2
anas-ikham <132776107+anas-ikham@users.noreply.github.com>
2024-09-05 19:41:51 +0100

    SQL Queries

---
+++ SQL Queries Churn.sql
@@ -0,0 +1,181 @@
+USE db_churn;
+Select * from stg_churn;
+Select gender, count(gender) as totalgender,
+		count(gender) * 100 / (select count(*) from  stg_churn) as pct_gender
+from stg_churn
+group by gender;
+
+Select Contract, count(Contract) as total_contract,
+		count(Contract) * 100 / (select count(*) from  stg_churn) as pct_contract
+from stg_churn
+GROUP BY contract;
+
+Select Customer_Status, count(Customer_Status) as totalcount, round(sum(total_revenue), 2) as totalRev,
+		round(sum(total_revenue) / (select sum(total_revenue) from stg_churn) * 100 , 2) as Pct_Rev
+from stg_churn
+group by Customer_Status;
+
+SELECT State, Count(State) as TotalCount,
+Count(State) * 100 / (Select Count(*) from stg_Churn)  as Percentage
+from stg_Churn
+Group by State
+Order by Percentage desc;
+
+Select DISTINCT internet_type
+from stg_Churn;
+
+-- Data Exploration – Check Nulls:
+
+SELECT 
+    SUM(CASE WHEN Customer_ID IS NULL THEN 1 ELSE 0 END) AS Customer_ID_Null_Count,
+    SUM(CASE WHEN Gender IS NULL THEN 1 ELSE 0 END) AS Gender_Null_Count,
+    SUM(CASE WHEN Age IS NULL THEN 1 ELSE 0 END) AS Age_Null_Count,
+    SUM(CASE WHEN Married IS NULL THEN 1 ELSE 0 END) AS Married_Null_Count,
+    SUM(CASE WHEN State IS NULL THEN 1 ELSE 0 END) AS State_Null_Count,
+    SUM(CASE WHEN Number_of_Referrals IS NULL THEN 1 ELSE 0 END) AS Number_of_Referrals_Null_Count,
+    SUM(CASE WHEN Tenure_in_Months IS NULL THEN 1 ELSE 0 END) AS Tenure_in_Months_Null_Count,
+    SUM(CASE WHEN Value_Deal IS NULL THEN 1 ELSE 0 END) AS Value_Deal_Null_Count,
+    SUM(CASE WHEN Phone_Service IS NULL THEN 1 ELSE 0 END) AS Phone_Service_Null_Count,
+    SUM(CASE WHEN Multiple_Lines IS NULL THEN 1 ELSE 0 END) AS Multiple_Lines_Null_Count,
+    SUM(CASE WHEN Internet_Service IS NULL THEN 1 ELSE 0 END) AS Internet_Service_Null_Count,
+    SUM(CASE WHEN Internet_Type IS NULL THEN 1 ELSE 0 END) AS Internet_Type_Null_Count,
+    SUM(CASE WHEN Online_Security IS NULL THEN 1 ELSE 0 END) AS Online_Security_Null_Count,
+    SUM(CASE WHEN Online_Backup IS NULL THEN 1 ELSE 0 END) AS Online_Backup_Null_Count,
+    SUM(CASE WHEN Device_Protection_Plan IS NULL THEN 1 ELSE 0 END) AS Device_Protection_Plan_Null_Count,
+    SUM(CASE WHEN Premium_Support IS NULL THEN 1 ELSE 0 END) AS Premium_Support_Null_Count,
+    SUM(CASE WHEN Streaming_TV IS NULL THEN 1 ELSE 0 END) AS Streaming_TV_Null_Count,
+    SUM(CASE WHEN Streaming_Movies IS NULL THEN 1 ELSE 0 END) AS Streaming_Movies_Null_Count,
+    SUM(CASE WHEN Streaming_Music IS NULL THEN 1 ELSE 0 END) AS Streaming_Music_Null_Count,
+    SUM(CASE WHEN Unlimited_Data IS NULL THEN 1 ELSE 0 END) AS Unlimited_Data_Null_Count,
+    SUM(CASE WHEN Contract IS NULL THEN 1 ELSE 0 END) AS Contract_Null_Count,
+    SUM(CASE WHEN Paperless_Billing IS NULL THEN 1 ELSE 0 END) AS Paperless_Billing_Null_Count,
+    SUM(CASE WHEN Payment_Method IS NULL THEN 1 ELSE 0 END) AS Payment_Method_Null_Count,
+    SUM(CASE WHEN Monthly_Charge IS NULL THEN 1 ELSE 0 END) AS Monthly_Charge_Null_Count,
+    SUM(CASE WHEN Total_Charges IS NULL THEN 1 ELSE 0 END) AS Total_Charges_Null_Count,
+    SUM(CASE WHEN Total_Refunds IS NULL THEN 1 ELSE 0 END) AS Total_Refunds_Null_Count,
+    SUM(CASE WHEN Total_Extra_Data_Charges IS NULL THEN 1 ELSE 0 END) AS Total_Extra_Data_Charges_Null_Count,
+    SUM(CASE WHEN Total_Long_Distance_Charges IS NULL THEN 1 ELSE 0 END) AS Total_Long_Distance_Charges_Null_Count,
+    SUM(CASE WHEN Total_Revenue IS NULL THEN 1 ELSE 0 END) AS Total_Revenue_Null_Count,
+    SUM(CASE WHEN Customer_Status IS NULL THEN 1 ELSE 0 END) AS Customer_Status_Null_Count,
+    SUM(CASE WHEN Churn_Category IS NULL THEN 1 ELSE 0 END) AS Churn_Category_Null_Count,
+    SUM(CASE WHEN Churn_Reason IS NULL THEN 1 ELSE 0 END) AS Churn_Reason_Null_Count
+FROM stg_Churn;
+
+-- Remove null and insert the new data into Prod table:
+
+CREATE TABLE prod_Churn AS
+SELECT 
+    Customer_ID,
+    Gender,
+    Age,
+    Married,
+    State,
+    Number_of_Referrals,
+    Tenure_in_Months,
+    COALESCE(Value_Deal, 'None') AS Value_Deal,
+    Phone_Service,
+    COALESCE(Multiple_Lines, 'No') AS Multiple_Lines,
+    Internet_Service,
+    COALESCE(Internet_Type, 'None') AS Internet_Type,
+    COALESCE(Online_Security, 'No') AS Online_Security,
+    COALESCE(Online_Backup, 'No') AS Online_Backup,
+    COALESCE(Device_Protection_Plan, 'No') AS Device_Protection_Plan,
+    COALESCE(Premium_Support, 'No') AS Premium_Support,
+    COALESCE(Streaming_TV, 'No') AS Streaming_TV,
+    COALESCE(Streaming_Movies, 'No') AS Streaming_Movies,
+    COALESCE(Streaming_Music, 'No') AS Streaming_Music,
+    COALESCE(Unlimited_Data, 'No') AS Unlimited_Data,
+    Contract,
+    Paperless_Billing,
+    Payment_Method,
+    Monthly_Charge,
+    Total_Charges,
+    Total_Refunds,
+    Total_Extra_Data_Charges,
+    Total_Long_Distance_Charges,
+    Total_Revenue,
+    Customer_Status,
+    COALESCE(Churn_Category, 'Others') AS Churn_Category,
+    COALESCE(Churn_Reason, 'Others') AS Churn_Reason
+FROM stg_Churn
+WHERE 1=0;
+
+INSERT INTO prod_Churn (
+    Customer_ID,
+    Gender,
+    Age,
+    Married,
+    State,
+    Number_of_Referrals,
+    Tenure_in_Months,
+    Value_Deal,
+    Phone_Service,
+    Multiple_Lines,
+    Internet_Service,
+    Internet_Type,
+    Online_Security,
+    Online_Backup,
+    Device_Protection_Plan,
+    Premium_Support,
+    Streaming_TV,
+    Streaming_Movies,
+    Streaming_Music,
+    Unlimited_Data,
+    Contract,
+    Paperless_Billing,
+    Payment_Method,
+    Monthly_Charge,
+    Total_Charges,
+    Total_Refunds,
+    Total_Extra_Data_Charges,
+    Total_Long_Distance_Charges,
+    Total_Revenue,
+    Customer_Status,
+    Churn_Category,
+    Churn_Reason
+)
+SELECT 
+    Customer_ID,
+    Gender,
+    Age,
+    Married,
+    State,
+    Number_of_Referrals,
+    Tenure_in_Months,
+    COALESCE(Value_Deal, 'None') AS Value_Deal,
+    Phone_Service,
+    COALESCE(Multiple_Lines, 'No') AS Multiple_Lines,
+    Internet_Service,
+    COALESCE(Internet_Type, 'None') AS Internet_Type,
+    COALESCE(Online_Security, 'No') AS Online_Security,
+    COALESCE(Online_Backup, 'No') AS Online_Backup,
+    COALESCE(Device_Protection_Plan, 'No') AS Device_Protection_Plan,
+    COALESCE(Premium_Support, 'No') AS Premium_Support,
+    COALESCE(Streaming_TV, 'No') AS Streaming_TV,
+    COALESCE(Streaming_Movies, 'No') AS Streaming_Movies,
+    COALESCE(Streaming_Music, 'No') AS Streaming_Music,
+    COALESCE(Unlimited_Data, 'No') AS Unlimited_Data,
+    Contract,
+    Paperless_Billing,
+    Payment_Method,
+    Monthly_Charge,
+    Total_Charges,
+    Total_Refunds,
+    Total_Extra_Data_Charges,
+    Total_Long_Distance_Charges,
+    Total_Revenue,
+    Customer_Status,
+    COALESCE(Churn_Category, 'Others') AS Churn_Category,
+    COALESCE(Churn_Reason, 'Others') AS Churn_Reason
+FROM stg_Churn;
+
+-- Create View for Power BI
+Create View vw_ChurnData as
+	select * from prod_Churn where Customer_Status In ('Churned', 'Stayed');
+    
+Create View vw_JoinData as
+	select * from prod_Churn where Customer_Status = 'Joined';
+
+
+
+		
